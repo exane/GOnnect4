14acb0c094c898bacabbb75fcb3df2468dddbe5d
Tim Meier <raco0n@gmx.de>
2016-04-13 08:09:24 +0200

    add some experimental go routines

--- src/connectfour/Game.go
+++ src/connectfour/Game.go
@@ -142,3 +142,156 @@ func (g *Game) checkWinDirection(p Point, direction, depth, currPlayer int) bool
   }
   return false
 }
+
+//-------------------
+
+func (g *Game) checkWinConditionGo(currPlayer int) bool {
+  ch := make(chan bool, len(g.board.fields))
+  for i := range g.board.fields {
+    p := Point{}
+    p.fromIndex(i)
+    go g.checkWinDirectionsGo(p, currPlayer, ch)
+  }
+
+  for i := 0; i < len(g.board.fields); i++ {
+    if <-ch {
+      return true
+    }
+  }
+
+  return false
+}
+
+func (g *Game) checkWinDirectionsGo(p Point, currPlayer int, ch chan bool) {
+  ch2 := make(chan bool, 4)
+
+  for i := 0; i < 4; i++ {
+    go g.checkWinDirectionGo(p, i, 0, currPlayer, ch2)
+  }
+
+  for i := 0; i < 4; i++ {
+    if <-ch2 {
+      ch <- true
+      return
+    }
+  }
+  ch <- false
+
+}
+
+func (g *Game) checkWinDirectionGo(p Point, direction, depth, currPlayer int, ch chan bool) {
+  player := PLAYER[currPlayer]
+
+  if v, ok := g.board.get(p); v != player || !ok {
+    ch <- false
+    return
+  }
+
+  if depth >= WINCONDITION - 1 {
+    ch <- true
+    return
+  }
+
+  switch direction {
+  case RIGHT:
+    p.x++
+    g.checkWinDirectionGo(p, RIGHT, depth + 1, currPlayer, ch)
+    return
+    break
+  case DOWN:
+    p.y++
+    g.checkWinDirectionGo(p, DOWN, depth + 1, currPlayer, ch)
+    return
+    break
+  case DIAG_UP:
+    p.x++
+    p.y++
+    g.checkWinDirectionGo(p, DIAG_UP, depth + 1, currPlayer, ch)
+    return
+    break
+  case DIAG_DOWN:
+    p.x++
+    p.y--
+    g.checkWinDirectionGo(p, DIAG_DOWN, depth + 1, currPlayer, ch)
+    return
+    break
+  }
+  ch <- false
+  return
+}
+
+func (g *Game) checkWinConditionGo2(currPlayer int) bool {
+  ch := make(chan bool, len(g.board.fields))
+  for i := range g.board.fields {
+    p := Point{}
+    p.fromIndex(i)
+    go g.checkWinDirectionsGo2(p, currPlayer, ch)
+  }
+
+  for i := 0; i < len(g.board.fields); i++ {
+    if <-ch {
+      return true
+    }
+  }
+
+  return false
+}
+
+func (g *Game) checkWinDirectionsGo2(p Point, currPlayer int, ch chan bool) {
+
+  ch2 := make(chan bool, 4)
+
+  for i := 0; i < 4; i++ {
+    go g.checkWinDirectionGo2(p, i, 0, currPlayer, ch2)
+  }
+
+  for i := 0; i < 4; i++ {
+    if <-ch2 {
+      ch <- true
+      return
+    }
+  }
+  ch <- false
+
+}
+
+func (g *Game) checkWinDirectionGo2(p Point, direction, depth, currPlayer int, ch chan bool) {
+  player := PLAYER[currPlayer]
+
+  if v, ok := g.board.get(p); v != player || !ok {
+    ch <- false
+    return
+  }
+
+  if depth >= WINCONDITION - 1 {
+    ch <- true
+    return
+  }
+
+  switch direction {
+  case RIGHT:
+    p.x++
+    go g.checkWinDirectionGo2(p, RIGHT, depth + 1, currPlayer, ch)
+    return
+    break
+  case DOWN:
+    p.y++
+    go g.checkWinDirectionGo2(p, DOWN, depth + 1, currPlayer, ch)
+    return
+    break
+  case DIAG_UP:
+    p.x++
+    p.y++
+    go g.checkWinDirectionGo2(p, DIAG_UP, depth + 1, currPlayer, ch)
+    return
+    break
+  case DIAG_DOWN:
+    p.x++
+    p.y--
+    go g.checkWinDirectionGo2(p, DIAG_DOWN, depth + 1, currPlayer, ch)
+    return
+    break
+  }
+  ch <- false
+  return
+}
